8e2d71cf13461674d8aa39744e830343cd1c2913
Nils Plettenberg <nils.plettenberg@rwth-aachen.de>
2019-06-27 12:46:08 +0200

    created notebook

---
+++ algowatch-datenspende.py
@@ -0,0 +1,63 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.2'
+#       jupytext_version: 1.1.7
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+#import packages
+import json
+import os.path
+import pandas as pd
+import numpy as np
+
+# %%
+# create system specific path to json file
+path = os.path.join(
+    'data', 
+    'datenspende_btw17_public_data_2017-09-29.json'
+)
+
+# read json file
+with open(path, 'r') as json_file:
+    json_data = json.load(json_file)
+
+# %%
+# separate result lists from meta data
+result_lists = json_data[-1] 
+meta_data = json_data[0:(len(json_data)-1)]
+
+# create data frame with meta data
+df = pd.DataFrame(meta_data[0])
+df['person_id'] = pd.Series(
+    np.repeat(np.array([1]), df.shape[0], axis=0),
+    index=df.index
+)
+for i in range(1, len(meta_data)-1):
+    tmp = pd.DataFrame(meta_data[i])
+    tmp['person_id'] = pd.Series(
+        np.repeat(np.array([i]), tmp.shape[0], axis=0),
+        index=tmp.index
+    )
+    df = pd.concat([df, tmp])
+    
+print(df.head())
+
+# %%
+# filter meta data frame
+meta_data_df = df[df.search_type == "news"]
+meta_data_df = meta_data_df.reset_index()
+meta_data_df = meta_data_df.drop(
+    ['plugin_id', 'index'], axis=1
+)
+
+# %%
