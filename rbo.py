41b6ae64b82295e653d24837d709daaffb2a3421
Nils Plettenberg <nils.plettenberg@rwth-aachen.de>
2019-07-19 09:07:08 +0200

    copright statement in rbo.py

--- rbo.py
+++ rbo.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
 """Rank-biased overlap, a ragged sorted list similarity measure.
+
+Copyright (C)  2016 ÚČNK/David Lukeš
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 See http://doi.acm.org/10.1145/1852102.1852106 for details. All functions
 directly corresponding to concepts from the paper are named so that they can be
 clearly cross-identified.
