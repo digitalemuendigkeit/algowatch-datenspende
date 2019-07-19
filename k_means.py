d0008765f7ebef71229d1ff24109085414477fed
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2019-07-19 12:13:05 +0200

    minor changes

--- k_means.py
+++ k_means.py
@@ -8,17 +8,19 @@ import pandas as pd
 def k_means_rbo(lol, k, n_iterations):
     """k-means algorithm adjusted for list clustering"""
 
+    res_tmp = lol
+
     # choose k centroids
-    centroids = np.random.randint(0, len(lol), k)
-    
+    centroids = np.random.randint(0, len(res_tmp), k)
+
     # create an index list and drop the indices of the centroids
-    idx = [elem for elem in range(len(lol))]
+    idx = [elem for elem in range(len(res_tmp))]
     for k_idx in centroids:
         idx.pop(k_idx)
     
     # initialize clusters
-    clusters = np.repeat('NA', len(lol))
-    counter = 1
+    clusters = np.repeat('NA', len(res_tmp))
+    counter = 0
     for c in centroids:
         clusters[c] = counter
         counter = counter + 1
@@ -29,9 +31,11 @@ def k_means_rbo(lol, k, n_iterations):
         for i in idx:
             rbo_dist = []
             for c in centroids:
-                rbo_dist.append(rbo.rbo_ext(res[i], res[c], 0.9))
+                a = res_tmp(i)
+                b = res_tmp(c)
+                rbo_dist.append(rbo.rbo_ext(a, b, 0.9))
             
-            clusters[i] = rbo_dist.index(min(rbo_dist)) + 1
+            clusters[i] = rbo_dist.index(min(rbo_dist))
     
     df = pd.DataFrame(
         {
