a8d88aa7cdbeeff07152e6cb546b711172dcd621
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2019-12-19 11:06:26 +0100

    init analysis

--- plots.R
+++ plots.R
@@ -3,17 +3,17 @@
 library(here)
 library(feather)
 library(tidyverse)
-list.filenames<-list.files(here("workingData"),pattern=".feather$")
+list.filenames<-list.files(here("..", "workingData"),pattern=".feather$")
 
 all.keywords <- data.frame()
 for (i in 1:length(list.filenames))
 {
-  df <- read_feather(here("workingData", list.filenames[i]))
+  df <- read_feather(here("..", "workingData", list.filenames[i]))
   all.keywords <- rbind(all.keywords, df)
 }
 
 all.keywords %>% ggplot() +
   aes(x= timestamp, y= cluster) +
-  geom_point(shape=16, alpha= 0.3) +
+  geom_jitter(shape=16, alpha= 0.05) +
   facet_wrap(. ~ keyword) +
   NULL 
